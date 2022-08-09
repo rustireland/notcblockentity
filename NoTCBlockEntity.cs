d5988991fb0606e19ba35417bef99309ac2c53c4
Agamemnon <neil.a.rankin@googlemail.com>
2022-08-10 00:49:16 +0100

    Initial commit

---
+++ NoTCBlockEntity.cs
@@ -0,0 +1,145 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Oxide.Core;
+using Oxide.Core.Plugins;
+using Oxide.Core.Libraries.Covalence;
+
+namespace Oxide.Plugins
+{
+    [Info("NoTCBlockEntity", "Agamemnon", "1.0.0")]
+    [Description("Blocks players from placing certain entities unless they are in the vicinity of a Tool Cupboard.")]
+    class NoTCBlockEntity : RustPlugin
+    {
+        private ConfigData _configData;
+        private const string _permBypass = "notcblockentity.bypass";
+
+        #region Oxide Hooks
+        private void OnServerInitialized()
+        {
+            permission.RegisterPermission(_permBypass, this);
+
+            if (!LoadConfigVariables())
+            {
+                PrintError("ERROR: The config file is corrupt. Either fix or delete it and restart the plugin.");
+                PrintError("ERROR: Unloading plugin.");
+                Interface.Oxide.UnloadPlugin(this.Title);
+                return;
+            }
+
+        }
+
+        private object CanBuild(Planner planner, Construction prefab, Construction.Target target)
+        {
+            if (!permission.UserHasPermission(planner.GetOwnerPlayer().UserIDString, _permBypass))
+            {
+                var cupboard = planner.GetOwnerPlayer().GetBuildingPrivilege();
+                if (cupboard == null)
+                {
+                    string shortname = GetShortname(prefab.fullName);
+                    shortname = Regex.Replace(shortname, ".deployed", "");
+                    shortname = Regex.Replace(shortname, "_deployed", "");
+    
+                    foreach (string entity in _configData.BlockedEntities)
+                    {
+                        if (String.Equals(shortname, entity))  
+                        {
+                            SendReply(planner.GetOwnerPlayer(), Lang("BlockedMessage",
+                                new KeyValuePair<string, string>("entity", entity)), _configData.ChatIcon);
+                            return false;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+        #endregion
+
+        #region Helper Functions
+        private static string GetShortname(string original)
+        {
+            var index = original.LastIndexOf("/", StringComparison.Ordinal) + 1;
+            var name = original.Substring(index);
+            return name.Replace(".prefab", string.Empty);
+        }
+        #endregion
+
+        #region Configuration
+        private class ConfigData
+        {
+            [JsonProperty(PropertyName = "Chat Icon (SteamID)")]
+            public ulong ChatIcon { get; private set; } = 0;
+
+            [JsonProperty(PropertyName = "Entities to Block (use shortnames)")]
+            public List<string> BlockedEntities {get; private set; }
+
+            public static ConfigData CreateDefault()
+            {
+                return new ConfigData
+                {
+                    BlockedEntities = new List<string>
+                    {
+                        "autoturret",
+                        "bbq",
+                        "mixingtable"
+                    }
+                };
+            }
+        }
+
+        private bool LoadConfigVariables()
+        {
+            try
+            {
+                _configData = Config.ReadObject<ConfigData>();
+            }
+            catch
+            {
+                return false;
+            }
+
+            SaveConfig(_configData);
+            return true;
+        }
+
+        protected override void LoadDefaultConfig()
+        {
+            PrintWarning("Creating new config file.");
+            _configData = ConfigData.CreateDefault();
+            SaveConfig(_configData);
+        }
+
+        private void SaveConfig(ConfigData config)
+        {
+            Config.WriteObject(config, true);
+        }
+        #endregion
+
+        #region Language
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["BlockedMessage"] = "<color=#ff6969>WARNING:</color> You must be within range of a <color=#ffa500>Tool Cupboard</color> to place a <color=#ffa500>{entity}</color>!"
+
+            }, this);
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["BlockedMessage"] = "<color=#ff6969>ATTENTION:</color> Vous devez être à portée d'une <color=#ffa500>Armoire à Outils</color> pour placer une <color=#ffa500>{entity}</color>!"
+            }, this, "fr");
+        }
+
+        private string Lang(string key) => string.Format(lang.GetMessage(key, this));
+        private string Lang(string key, params KeyValuePair<string, string>[] replacements)
+        {
+            var message = lang.GetMessage(key, this);
+
+            foreach (var replacement in replacements)
+                message = message.Replace($"{{{replacement.Key}}}", replacement.Value);
+
+            return message;
+        }
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,54 @@
+# NoTCBlockEntity
+**NoTCBlockEntity** is an [Oxide](https://umod.org/) plugin that blocks players from placing certain entities unless they are in the vicinity of a Tool Cupboard. Its primary purpose is to help address some of the limitations of the [EntityLimit](https://umod.org/plugins/entity-limit) plugin.
+### Table of Contents  
+- [Requirements](#requirements)  
+- [Installation](#installation)  
+- [Permissions](#permissions)  
+- [Commands](#commands)  
+- [Configuration](#configuration)  
+- [Localization](#localization)  
+- [Credits](#credits)
+## Requirements
+| Depends On | Works With |
+| --- | --- |
+| None | [EntityLimit](https://umod.org/plugins/entity-limit) |
+## Installation
+Download the plugin:
+```bash
+git clone https://github.com/rustireland/notcblockentity.git
+```
+Copy it to the Oxide plugins directory:
+```bash
+cp notcblockentity/NoTCBlockEntity.cs oxide/plugins
+```
+Oxide will compile and load the plugin automatically.
+## Permissions
+This plugin uses the Oxide permission system. To assign a permission, use `oxide.grant <user or group> <name or steam id> <permission>`. To remove a permission, use `oxide.revoke <user or group> <name or steam id> <permission>`.
+- `notcblockentity.bypass` - Allows a user or group to bypass all blocks on entity deployment
+## Commands
+This plugin doesn't provide any console or chat commands.
+## Configuration
+The settings and options can be configured in the `NoTCBlockEntity.json` file under the `oxide/config` directory. The use of an editor and validator is recommended to avoid formatting issues and syntax errors.
+
+When run for the first time, the plugin will create a default configuration file with *Auto Turrets*, *BBQ's*, and *Mixing Tables* blocked.
+```json
+{
+  "Chat Icon (SteamID)": 0,
+  "Entities to Block (use shortnames)": [
+    "autoturret",
+    "bbq",
+    "mixingtable"
+  ]
+}
+```
+## Localization
+The default messages are in the `NoTCBlockEntity.json` file under the `oxide/lang/en` directory. To add support for another language, create a new language folder (e.g. **de** for German) if not already created, copy the default language file to the new folder and then customize the messages.
+```json
+{
+  "BlockedMessage": "<color=#ff6969>WARNING:</color> You must be within range of a <color=#ffa500>Tool Cupboard</color> to place a <color=#ffa500>{entity}</
+color>!"
+}
+```
+# Credits
+- [Agamemnon](https://github.com/agamemnon23) - Code, testing.
+- [Black_demon6](https://github.com/TheBlackdemon6) - Initial concept, testing, and French translations.
